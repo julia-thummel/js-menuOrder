ecd4dffff14b91728febd85d5875f8ef4536d8d2
julia-thummel <julia.thummel@wustl.edu>
2016-04-18 18:59:55 -0500

    g

--- menu.js
+++ menu.js
@@ -1,15 +1,31 @@
 // begin with a blank/new order
 var order = '';
+var l = 'lettuce';
 
 
 // build a function to ask questions
+function question(questiontext) {
+  //what our function will do
+  var answerinput = prompt(questiontext);
+  order = order + ", " + answerinput;
 
-function question() {
-  // your code goes here
-}
+} // taco, black beans, chicken, white rice, peppers
+function questionYN(ingredient) {
+  //what our function will do
+  var myPrompt = "Do you want: " + ingredient + "?";
+  var answerinput = prompt(myPrompt);
+  if (answerinput.toUpperCase() === "YES"){
+    order = order + ", " + ingredient;
+  }
+} // taco, black beans, chicken, white rice, peppers
 
 // call the QUESTION function with the order options
-question();
+question("Do you want a burrito, a bowl, or a taco?");
+question("Do you want black beans or pinto beans?");
+question("Do you want chicken, beef, or carnitas?");
+question("Do you want white rice or brown rice?");
+questionYN("lettuce");
+questionYN("guac");
 
 // output their order
 document.write("<p>You ordered: " + order + "<p>");
